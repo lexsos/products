64e82a2b75677a04fff68888c3ce78317cdf1121
lexsos <ale-sosnin@yandex.ru>
2014-08-26 07:30:13 +0400

    Add models Shop, Product, Cost

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
 
-from .models import Category
+from .models import Category, Shop, Product, Cost
 
 
 class CategoryAdmin(MPTTModelAdmin):
-    list_filter = ('parent', )
+    list_display = ('title', 'weight', 'enabled')
+
+
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('title', 'address', 'weight', 'enabled')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'enabled')
+
+
+class CostAdmin(admin.ModelAdmin):
+    list_display = ('product', 'shop', 'price')
 
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Shop, ShopAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Cost, CostAdmin)
--- apps/products/models.py
+++ apps/products/models.py
@@ -16,14 +16,108 @@ class Category(models.Model):
         _('title'),
         max_length=255,
     )
+    weight = models.PositiveIntegerField(
+        verbose_name=_('weight'),
+        default=0,
+        help_text=_('Rocord will be first with greate weight.'),
+    )
+    enabled = models.BooleanField(
+        verbose_name=_('enabled'),
+        default=True,
+        help_text=_('show/hide'),
+    )
 
     class Meta:
         verbose_name = _(u'category item')
         verbose_name_plural = _(u'category items')
-        ordering = ['title', ]
+        ordering = ['weight', 'title']
 
     def __unicode__(self):
         return self.title
+
+
+class Shop(models.Model):
+
+    title = models.CharField(
+        _('title'),
+        max_length=255,
+    )
+    address = models.CharField(
+        _('title'),
+        max_length=255,
+    )
+    weight = models.PositiveIntegerField(
+        verbose_name=_('weight'),
+        default=0,
+        help_text=_('Rocord will be first with greate weight.'),
+    )
+    enabled = models.BooleanField(
+        verbose_name=_('enabled'),
+        default=True,
+        help_text=_('show/hide'),
+    )
+
+    class Meta:
+        verbose_name = _(u'shop item')
+        verbose_name_plural = _(u'shop items')
+        ordering = ['weight', 'title']
+
+    def __unicode__(self):
+        return self.title
+
+
+class Product(models.Model):
+
+    title = models.CharField(
+        _('title'),
+        max_length=255,
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name=_('category item'),
+    )
+    weight = models.PositiveIntegerField(
+        verbose_name=_('weight'),
+        default=0,
+        help_text=_('Rocord will be first with greate weight.'),
+    )
+    enabled = models.BooleanField(
+        verbose_name=_('enabled'),
+        default=True,
+        help_text=_('show/hide'),
+    )
+
+    class Meta:
+        verbose_name = _(u'product item')
+        verbose_name_plural = _(u'product items')
+        ordering = ['weight', 'title']
+
+    def __unicode__(self):
+        return self.title
+
+
+class Cost(models.Model):
+
+    product = models.ForeignKey(
+        Product,
+        verbose_name=_('product item'),
+    )
+    shop = models.ForeignKey(
+        Shop,
+        verbose_name=_('shop item'),
+    )
+    price = models.FloatField(
+        verbose_name=_('price'),
+    )
+
+    class Meta:
+        verbose_name = _(u'cost item')
+        verbose_name_plural = _(u'cost items')
+        ordering = ['product', 'shop']
+        unique_together = ('product', 'shop')
+
+    def __unicode__(self):
+        return u'{0}:{1}'.format(self.product.title, self.shop.title)
 
 
 mptt.register(Category,)
